fd930b4e7d771a5c401bf0aca6cae3bf938ffa49
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-18 19:53:44 -0800

    added setupInput & handleUserInput

--- play.js
+++ play.js
@@ -2,5 +2,22 @@ const net = require("net");
 const connect = require('./client')
 // establishes a connection with the game server
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    if (key === '\u0003'){
+      process.exit();
+    }
+  }) 
+};
+
+stdin.on("data", handleUserInput);
 console.log("Connecting ...");
 connect();
