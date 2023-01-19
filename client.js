3cf69c7780afe329040e6611ba01fe065cd8c982
Ethan Wong <ethanwong98@yahoo.ca>
2023-01-18 22:33:11 -0800

    finished snake game

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
+    IP: "localhost",
+    PORT: 50541,
   });
 
   conn.setEncoding("utf8");
